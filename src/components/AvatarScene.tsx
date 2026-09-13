e50ab4942b5db5c28ec3ec8cd52f15448dead5c7
anwarrahahlehSG <anwar.rahahleh@sahamgate.com>
2026-09-13 15:16:51 +0400

    Improve lighting for reference-style companion

--- src/components/AvatarScene.tsx
+++ src/components/AvatarScene.tsx
@@ -5,14 +5,23 @@ import type { CompanionState, Outfit } from '../lib/types'
 
 export function AvatarScene(props: { state: CompanionState; outfit: Outfit; onPoke?: () => void }) {
   return (
-    <Canvas camera={{ position: [0, 0.55, 5.8], fov: 38 }} shadows dpr={[1, 1.5]}>
-      <ambientLight intensity={1.35} />
-      <directionalLight position={[3, 5, 4]} intensity={2.2} castShadow />
-      <pointLight position={[-3, 2, 3]} intensity={0.7} color="#72c7ff" />
+    <Canvas camera={{ position: [0, .62, 5.45], fov: 36 }} shadows dpr={[1, 1.75]}>
+      <ambientLight intensity={1.65} />
+      <directionalLight position={[3.5, 5.5, 4.5]} intensity={2.4} castShadow />
+      <directionalLight position={[-4, 2.8, 2]} intensity={1.0} color="#cfe7ff" />
+      <pointLight position={[0, 1.7, 3]} intensity={.65} color="#8edbff" />
       <Robot {...props} />
-      <ContactShadows position={[0, -1.08, 0]} opacity={0.32} scale={5} blur={2.5} />
+      <ContactShadows position={[0, -1.18, 0]} opacity={.30} scale={4.8} blur={3.0} />
       <Environment preset="city" />
-      <OrbitControls enableZoom={false} enablePan={false} target={[0, 0.3, 0]} minPolarAngle={1.25} maxPolarAngle={1.9} minAzimuthAngle={-0.5} maxAzimuthAngle={0.5} />
+      <OrbitControls
+        enableZoom={false}
+        enablePan={false}
+        target={[0, .35, 0]}
+        minPolarAngle={1.25}
+        maxPolarAngle={1.85}
+        minAzimuthAngle={-.42}
+        maxAzimuthAngle={.42}
+      />
     </Canvas>
   )
 }
